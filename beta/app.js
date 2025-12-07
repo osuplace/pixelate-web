52741f8339cfdbfdd86da5b1f5d43d371bac5573
Endrik Tombak <littleendu@gmail.com>
2025-12-07 20:26:13 +0200

    beta everything else

---
+++ beta/app.js
@@ -0,0 +1,483 @@
+/* global ort */
+let pixelateSession = null;
+let vertCombineSession = null;
+let horiCombineSession = null;
+let finalizerSession = null;
+let currentImageBitmap = null;
+let desiredSize = 0;
+let outputCanvas = document.createElement("canvas");
+let filePrepared = false;
+let downloadReady = false;
+let downloaded = false;
+let running = false;
+let estimatedTimePerPixel = 0;
+let runningExecutionProvider = "";
+let loadedPaletteBlobs = {}
+let paletteTensor = null;
+
+// Get the elements from the DOM
+const disclaimer = document.getElementById("disclaimer");
+const textOverlay = document.getElementById("text-overlay");
+let defaultOverlayText = "<p>Drag and drop an image here to upload</p><p>or</p><p>Click to select an image from your computer</p>";
+
+const paletteDropdown = document.getElementById("palette-dropdown");
+const scaleRange = document.getElementById("scale-range");
+const scaleNumber = document.getElementById("scale-number");
+
+const runButton = document.getElementById("run-button");
+const download1Button = document.getElementById("download-1-button");
+const download4Button = document.getElementById("download-4-button");
+const downloadDButton = document.getElementById("download-d-button");
+
+/** @type {HTMLCanvasElement} */
+const mainCanvas = document.getElementById("main-canvas");
+const fileInput = document.getElementById("file-input")
+const paletteInput = document.getElementById("palette-input")
+const canvasContainer = document.getElementById('canvas-container');
+
+let maxTileSize = 256; // size of the tiles to process
+let overlap = 32; // overlap between *input* tiles
+
+function setDownloadButtonsDisabledTo(disabled) {
+    download1Button.disabled = disabled;
+    download4Button.disabled = disabled;
+    downloadDButton.disabled = disabled;
+}
+
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function setTextOverlayInner(newHtml, updateVisibility = true) {
+    if (newHtml && updateVisibility && textOverlay.hidden) {
+        textOverlay.hidden = false;
+        await sleep(50);
+    } else if (newHtml.length === 0 && updateVisibility && !textOverlay.hidden) {
+        textOverlay.hidden = true;
+        await sleep(50);
+    }
+    textOverlay.innerHTML = newHtml;
+    await sleep(1);
+}
+
+class TextOverlayPercentageScheduler {
+    constructor() {
+        this.running = false;
+        this.current = 0;
+        this.speed = 0; // how long it takes for the percentage to increment
+    }
+
+    async start(currentPercentage, speed) {
+        this.current = currentPercentage;
+        this.speed = speed;
+        textOverlay.hidden = false;
+
+        if (runningExecutionProvider.includes('cpu')) {
+            // wasm and cpu will block the event loop, no need to start the scheduler
+            return;
+        }
+
+        while (this.speed === 0) {
+            await setTextOverlayInner(`<p>${this.current}%</p>`)
+            await sleep(1000)
+        }
+
+        this.running = true;
+        await setTextOverlayInner(`<p>${this.current}%</p>`, false);
+
+        while (this.current < 100) {
+            await sleep(this.speed);
+            this.current += 1;
+            await setTextOverlayInner(`<p>${this.current}%</p>`, false);
+            if (!this.running) {
+                await setTextOverlayInner("");
+                return;
+            }
+        }
+    }
+
+    async update(currentPercentage, speed) {
+        console.debug(`Updating percentage: ${currentPercentage}, Speed: ${speed}`);
+        this.current = currentPercentage;
+        this.speed = speed;
+        if (!this.running) {
+            await setTextOverlayInner(`<p>${currentPercentage}%</p>`, false);
+        }
+    }
+
+    stop() {
+        this.running = false;
+        setTextOverlayInner("").catch(console.error);
+    }
+}
+
+async function runCurrentFile() {
+    if (!filePrepared) return;
+    if (running) return;
+    running = true;
+    await setTextOverlayInner('Processing image...');
+
+    const maxSize = scaleRange.max;
+    let maxPossiblePower = Math.min(Math.floor(Math.log2(maxSize / desiredSize)), 3)
+    console.log(`Max possible power: ${maxPossiblePower}`);
+    let scale = Math.pow(2, maxPossiblePower);
+
+    let desiredWidth = Math.round(currentImageBitmap.width * desiredSize / maxSize)
+    let desiredHeight = Math.round(currentImageBitmap.height * desiredSize / maxSize)
+    let inputWidth = desiredWidth * scale;
+    let inputHeight = desiredHeight * scale;
+
+    let inputCanvas = document.createElement('canvas');
+    inputCanvas.width = inputWidth;
+    inputCanvas.height = inputHeight;
+    let inputCtx = inputCanvas.getContext('2d');
+    inputCtx.drawImage(currentImageBitmap, 0, 0, inputWidth, inputHeight);
+
+    // tiling
+    mainCanvas.width = inputWidth;
+    mainCanvas.height = inputHeight;
+    let ctx = mainCanvas.getContext("2d");
+    // draw the image, then disable image smoothing
+    ctx.drawImage(inputCanvas, 0, 0);
+    ctx.imageSmoothingEnabled = false;
+
+    let tileSize = Math.floor(maxTileSize / scale) * scale;
+    let tileOverlap = Math.floor(overlap / scale) * scale;
+    let halfTileSize = Math.floor(tileSize / scale / 2) * scale;
+
+    let heightRemaining = inputHeight;
+    let heightCombined = null
+
+    while (heightRemaining > 0) {
+        let widthRemaining = inputWidth;
+        let widthCombined = null
+
+        let tileHeight = Math.min(heightRemaining, tileSize);
+        let lastVTile = (heightRemaining === tileHeight);
+        if (heightRemaining - tileHeight + tileOverlap < halfTileSize && !lastVTile)
+            tileHeight = halfTileSize;
+        while (widthRemaining > 0) {
+            let tileWidth = Math.min(widthRemaining, tileSize);
+            let lastHTile = (widthRemaining === tileWidth);
+            if (widthRemaining - tileWidth + tileOverlap < halfTileSize && !lastHTile)
+                tileWidth = halfTileSize;
+            let tileX = inputWidth - widthRemaining;
+            let tileY = inputHeight - heightRemaining;
+
+
+
+            let tileCanvas = document.createElement('canvas');
+            tileCanvas.width = Math.min(tileWidth, widthRemaining + tileOverlap);
+            tileCanvas.height = Math.min(tileHeight, heightRemaining + tileOverlap);
+            let tileCtx = tileCanvas.getContext('2d');
+            tileCtx.drawImage(inputCanvas, tileX, tileY, tileWidth, tileHeight, 0, 0, tileWidth, tileHeight);
+
+            console.log(`Processing tile at (${tileX}, ${tileY}) with size ${tileWidth}x${tileHeight}`);
+            let tileTensor = await ort.Tensor.fromImage(tileCtx.getImageData(0, 0, tileCanvas.width, tileCanvas.height))
+            let tileOutput = await pixelateSession.run({
+                'image': tileTensor,
+                'palette': paletteTensor,
+                'downscale_power': new ort.Tensor('int64', new BigInt64Array([BigInt(maxPossiblePower)]), [])
+            });
+            tileTensor.dispose();
+
+            let finalOutput = await finalizerSession.run({
+                'probs': tileOutput['out_probs'],
+                'palette': paletteTensor,
+            });
+
+            let imageBitmap = await createImageBitmap(finalOutput['image'].toImageData());
+            ctx.drawImage(imageBitmap, 0, 0, tileWidth / scale, tileHeight / scale, tileX, tileY, tileWidth, tileHeight);
+
+            if (widthCombined === null)
+                widthCombined = tileOutput['out_probs']
+            else {
+                console.log("Combining horizontal tiles")
+                let combinedTensor = await horiCombineSession.run({
+                    'a': widthCombined,
+                    'b': tileOutput['out_probs'],
+                    'overlap': new ort.Tensor('int64', new BigInt64Array([BigInt(tileOverlap / scale)]), [1])
+                })
+                widthCombined = combinedTensor['out']
+            }
+
+            widthRemaining -= tileWidth - tileOverlap
+            console.log(`widthLeft: ${widthRemaining}`)
+            if (lastHTile) break;  // otherwise widthRemaining will always be tileOverlap
+            // TODO: update progress bar
+        }
+        if (heightCombined === null)
+            // noinspection JSSuspiciousNameCombination
+            heightCombined = widthCombined
+        else {
+            console.log("Combining vertical tiles")
+            let combinedTensor = await vertCombineSession.run({
+                'a': heightCombined,
+                'b': widthCombined,
+                'overlap': new ort.Tensor('int64', new BigInt64Array([BigInt(tileOverlap / scale)]), [1])
+            })
+            heightCombined = combinedTensor['out']
+        }
+        heightRemaining -= tileHeight - tileOverlap
+        if (lastVTile) break;
+    }
+    let finalOutput = await finalizerSession.run({
+        'probs': heightCombined,
+        'palette': paletteTensor,
+    });
+    let imageData = finalOutput['image'].toImageData();
+    console.log(imageData)
+    let imageBitmap = await createImageBitmap(imageData);
+    ctx.clearRect(0, 0, inputWidth, inputHeight);
+    ctx.drawImage(imageBitmap, 0, 0, desiredWidth, desiredHeight, 0, 0, inputWidth, inputHeight);
+
+    outputCanvas.width = desiredWidth;
+    outputCanvas.height = desiredHeight;
+    let outputCtx = outputCanvas.getContext('2d');
+    outputCtx.drawImage(imageBitmap, 0, 0, desiredWidth, desiredHeight);
+    await setTextOverlayInner("");
+}
+
+async function preventedFileUpload() {
+    if (running) {
+        return true;
+    }
+    if (downloadReady && !downloaded) {
+        await setTextOverlayInner("<p>Please download the image before running another image.</p>")
+        return true;
+    }
+    return false;
+}
+
+async function handleFileUpload(file) {
+    if (await preventedFileUpload()) return;
+
+    downloaded = false;
+    downloadReady = false;
+    filePrepared = false;
+    setDownloadButtonsDisabledTo(true);
+    await setTextOverlayInner('Loading image...');
+
+    currentImageBitmap = await createImageBitmap(file);
+    let maxSize = Math.max(currentImageBitmap.width, currentImageBitmap.height);
+    scaleRange.max = scaleNumber.max = maxSize;
+    scaleRange.value = scaleNumber.value = Math.min(300, maxSize / 2);
+    await setTextOverlayInner(paletteTensor != null ? "" : "<p>Select a palette from the bottom-left dropdown</p>");
+    defaultOverlayText = ""
+    await drawCurrentImage();
+    filePrepared = true;
+    runButton.disabled = false;
+}
+
+async function drawCurrentImage() {
+    if (!currentImageBitmap) return;
+
+    mainCanvas.width = currentImageBitmap.width;
+    mainCanvas.height = currentImageBitmap.height;
+    desiredSize = scaleRange.value;
+    const maxSize = scaleRange.max;
+
+    let scalingCanvas = document.createElement('canvas')
+    let sCtx = scalingCanvas.getContext('2d')
+    scalingCanvas.width = Math.round(currentImageBitmap.width * desiredSize / maxSize);
+    scalingCanvas.height = Math.round(currentImageBitmap.height * desiredSize / maxSize);
+    sCtx.drawImage(currentImageBitmap, 0, 0, scalingCanvas.width, scalingCanvas.height);
+
+    let ctx = mainCanvas.getContext("2d");
+    ctx.imageSmoothingEnabled = false;
+    ctx.drawImage(scalingCanvas, 0, 0, scalingCanvas.width, scalingCanvas.height, 0, 0, mainCanvas.width, mainCanvas.height);
+}
+
+/**
+ * @param {Blob} userPalette - optional user-provided palette blob
+ * @param {string} paletteName - name of the palette to load
+ */
+async function loadOrFetchPalette(userPalette = null, paletteName = null) {
+    if (!userPalette && !paletteName) {
+        console.error("loadOrFetchPalette called without a palette");
+        return null;
+    }
+
+    // use the most optimal way to get the palette blob
+    let paletteBlob = userPalette || loadedPaletteBlobs[paletteName]
+    if (!paletteBlob) {
+        let paletteResponse = await fetch(`./palettes/${paletteName}.png`);
+        if (!paletteResponse.ok) {
+            console.error(`Failed to fetch palette ${paletteResponse.url} - ${paletteResponse.statusText}`);
+            return null;
+        }
+        paletteBlob = await paletteResponse.blob();
+    }
+
+    // cache the palette blob
+    loadedPaletteBlobs[paletteName] = paletteBlob;
+
+    // create canvas to later get ImageData from
+    const imageBitmap = await createImageBitmap(paletteBlob);
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    canvas.width = imageBitmap.width;
+    canvas.height = imageBitmap.height;
+    ctx.drawImage(imageBitmap, 0, 0);
+
+    // convert to Tensor and reshape to (B, C, N)
+    const tensor = await ort.Tensor.fromImage(ctx.getImageData(0, 0, canvas.width, canvas.height))
+    console.log(`Loaded palette ${paletteName} with shape ${tensor.dims}`);
+    await setTextOverlayInner(defaultOverlayText)
+    paletteTensor = tensor.reshape([1, 3, canvas.height * canvas.width]);
+}
+
+async function initializeModel(url, eps = [["webnn", "webgpu"], ["wasm", "cpu"]]) {
+    let rv = null;
+
+    const modelResponse = await fetch(url, {'cache': 'force-cache'});
+    if (!modelResponse.ok) {
+        console.error(`Failed to fetch model`);
+        await setTextOverlayInner("<p>Failed to download model</p>");
+        return;
+    }
+    const modelBuffer = await modelResponse.arrayBuffer();
+
+    let shouldRunOnGPU = false
+
+    for (const ep of eps) {
+        if (ep.includes("webgpu")) shouldRunOnGPU = true;
+        try {
+            rv = await ort.InferenceSession.create(modelBuffer, {
+                executionProviders: [...ep],
+                graphOptimizationLevel: "all",
+            });
+            console.debug(`Session created with ${ep}`);
+            runningExecutionProvider = ep.join(", ");
+            if (!ep.includes("webgpu") && shouldRunOnGPU) {
+                disclaimer.innerHTML = "<p>⚠️ WebGPU failed. Model is running on CPU - this can cause performance issues.</p>"
+            }
+            break
+        } catch (e) {
+            console.error(`Failed to create session with ${ep}: ${e}`);
+        }
+    }
+
+    return rv
+}
+
+async function initializeAllModels() {
+    pixelateSession = await initializeModel('./onnx/model.onnx')
+    vertCombineSession = await initializeModel('./onnx/vertical_overlap.onnx', [["wasm", "cpu"]])
+    horiCombineSession = await initializeModel('./onnx/horizontal_overlap.onnx', [["wasm", "cpu"]])
+    finalizerSession = await initializeModel('./onnx/image_finalizer.onnx')
+}
+
+document.addEventListener("DOMContentLoaded", async function () {
+    // prevent leaving the page if the model is running
+    window.addEventListener('beforeunload', function (event) {
+        if (running || downloadReady && !downloaded) {
+            event.preventDefault();
+        }
+    })
+
+    // button events
+    runButton.addEventListener("click", async function () {
+        await runCurrentFile();
+    })
+    scaleRange.addEventListener("input", async function (_) {
+        scaleNumber.value = scaleRange.value;
+        await drawCurrentImage()  // TODO: debounce this
+    })
+    scaleNumber.addEventListener("change", async function (_) {
+        scaleRange.value = scaleNumber.value;
+        await drawCurrentImage()  // TODO: debounce this
+    })
+    // TODO: handle download buttons
+
+
+    // file input via drag and drop
+    canvasContainer.addEventListener('dragover', function (event) {
+        event.preventDefault();
+        canvasContainer.classList.add('dragover');
+        event.dataTransfer.dropEffect = 'copy';
+    });
+    canvasContainer.addEventListener('dragleave', function (_) {
+        canvasContainer.classList.remove('dragover');
+    });
+    canvasContainer.addEventListener('drop', async function (event) {
+        event.preventDefault();
+        canvasContainer.classList.remove('dragover');
+        const files = event.dataTransfer.files;
+        await handleFileUpload(files[0]);
+    });
+
+    // file input via click
+    canvasContainer.addEventListener('click', async function (_) {
+        if (await preventedFileUpload()) return;
+        fileInput.click();
+    });
+    fileInput.addEventListener("change", async function (event) {
+        const files = event.target.files;
+        await handleFileUpload(files[0]);
+    })
+
+    // palette input via click
+    paletteInput.addEventListener("change", async function (event) {
+        const files = event.target.files;
+        let name = files[0].name.replace(".png", "");
+        let existingOption = paletteDropdown.querySelector(`option[value="${name}"]`);
+        if (existingOption) {
+            existingOption.selected = true;
+        } else {
+            let newOption = document.createElement("option");
+            newOption.value = name;
+            newOption.textContent = name;
+            paletteDropdown.appendChild(newOption);
+            newOption.selected = true;
+        }
+
+        await loadOrFetchPalette(files[0], name);
+    })
+    // palette input via dropdown
+    paletteDropdown.addEventListener("change", async function (_) {
+        let currentURL = new URL(window.location.href);
+        let selectedPalette = paletteDropdown.options[paletteDropdown.selectedIndex].value;
+        currentURL.searchParams.set("palette", selectedPalette);
+        history.pushState({}, '', currentURL);
+        await loadOrFetchPalette(null, selectedPalette);
+    })
+
+    // functions to run after load
+    // palette input via URL parameter
+    let paletteFromParams = new URLSearchParams(window.location.search).get("palette");
+    if (paletteFromParams) {
+        // select that palette from the dropdown
+        console.log(`Loading palette ${paletteFromParams} from URL params`);
+        let paletteOption = paletteDropdown.querySelector(`option[value="${paletteFromParams}"]`);
+        if (paletteOption) {
+            paletteOption.selected = true;
+            await loadOrFetchPalette(null, paletteOption.value);
+        } else {
+            console.error(`Palette ${paletteFromParams} not found`);
+        }
+    }
+    // model initialization
+    await initializeAllModels();
+
+})
+
+
+/* TODO:
+[ ] - Tiling (based on session memory)
+[ ] - Nearest neighbor preview before running the model
+[ ] - real-time preview & debounce the preview
+[ ] - allow upload of palettes
+[ ] - Add a button to run the model
+[ ] - Download button for the output image
+[ ] - slider for the scale factor
+[x] - dropdown for the palette
+[ ] - drag and drop support
+[ ] - Add loading indicator during model processing
+[ ] - Implement error handling for failed model runs
+[ ] - Add image size validation and warnings
+[ ] - Add keyboard shortcuts for common actions
+[ ] - Save user preferences in local storage
+[ ] - Implement progress bar for large images
+[ ] - Create shareable URLs for specific settings
+ */
